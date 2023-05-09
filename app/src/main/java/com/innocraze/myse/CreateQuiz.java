1e2bccc590e91c3217d22e54ee706519bc18c649
TarunCore <112856864+TarunCore@users.noreply.github.com>
2023-05-09 19:43:42 +0530

    Quiz java file initiate

---
+++ app/src/main/java/com/innocraze/myse/CreateQuiz.java
@@ -0,0 +1,14 @@
+package com.innocraze.myse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_quiz);
+    }
+}
